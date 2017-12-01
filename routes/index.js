74a2ba8296e9e716e9aa716f3bfc6105bee9ce71
Kayla Braun <go1018bx@go.minneapolis.edu>
2017-12-01 15:59:21 -0600

    search filters

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ var sequelize = new Sequelize('cattle', 'kayla', 'Hank', {
     dialect: 'postgres',
 
     pool: {
-        max: 5,
+        max: 100,
         min: 0,
         acquire: 30000,
         idle: 10000
@@ -99,17 +99,8 @@ Treatment.belongsTo(Cow, {foreignKey: 'cowId'});
 //Cow.sync({force: true});
 //Drugs.sync({force: true});
 
-//Cow.create(  {
-//    description: 'black cow',
-//    dob: 5/12/2014,
-//    birthing: 'unassisted birth, unassisted nursing',
-//    eartag: 456,
-//    typeId: 1,
-//    damId: 1,
-//    sireId:
-//});
 //Types.create( {
-//    name: 'bull'
+//    name: 'steer'
 //});
 //Drugs.create({
 //    name: 'penicillin',
@@ -120,9 +111,15 @@ Treatment.belongsTo(Cow, {foreignKey: 'cowId'});
 router.get('/', function(req, res, next) {
   res.render('index');
 });
+//shows all cows or just the filtered ones depending on your search
 router.get('/homePage', function(req, res, next) {
-    Cow.all().then(function(cow){
-        res.render('home', {'cows' : cow})
+    var filter = {};
+    if (req.query.eartag) filter.eartag = req.query.eartag;
+    if (req.query.type) filter.typeId = req.query.type;
+    Cow.all({where: filter, include: [Types]}).then(function(cow){
+        Types.all().then(function(types){
+            res.render('home', {'cows' : cow, 'types' : types})
+        })
     });
 });
 router.get('/drugs', function(req, res, next) {
@@ -131,8 +128,8 @@ router.get('/drugs', function(req, res, next) {
     })
 });
 router.get('/addCow/:eartag', function(req, res, next) {
-    var eartag = req.params.eartag
-    console.log(eartag)
+    var eartag = req.params.eartag;
+    console.log(eartag);
     Types.all().then(function(type) {
         res.render('addCow', {'types': type, 'dameartag' : eartag})
     });
@@ -145,7 +142,7 @@ router.get('/cowPage/:id', function(req, res, next) {
             treatment.givendate = moment(treatment.dateGiven).format('M/D/YYYY');
             treatment.withdrawaldate = withdrawalDate.format('M/D/YYYY');
         });
-        Cow.findOne({where: { damId: cowId}}).then(function(calf){
+        Cow.findAll({where: { damId: cow.eartag}, include: [Types]}).then(function(calf){
             res.render('cowPage', { 'cow' : cow , 'calves' : calf});
         });
     });
@@ -159,7 +156,9 @@ router.get('/treatment', function(req, res, next) {
 });
 router.post('/login', function(req, res, next) {
     res.redirect('/homePage')
-
+});
+router.post('/all', function(req, res, next) {
+    res.redirect('/homePage')
 });
 router.post('/druglist', function(req,res,next) {
     res.redirect('drugs')
@@ -169,54 +168,24 @@ router.post('/newCow', function(req,res,next) {
     res.render('addCow', {"eartag" : dameartag})
 });
 router.post('/addCow', function(req, res, next) {
-    var dameartag = req.body.dameartag;
-    var sireeartag = req.body.sireeartag;
-    Cow.findOne({where: { eartag : dameartag}}).then(function(damcow){
-        Cow.findOne({where: { eartag : sireeartag}}).then(function(sirecow){
-            if(damcow && sirecow) {
-                var damId = damcow.cowId;
-                var sireId = sirecow.cowId;
-                var eartag = req.body.eartag;
-                var description = req.body.description;
-                var type = req.body.typeId;
-                var birthing = req.body.birth;
-                var dob = req.body.dob;
-                Cow.create({
-                    description: description,
-                    dob: dob,
-                    birthing: birthing,
-                    eartag: eartag,
-                    typeId: type,
-                    damId: damId,
-                    sireId: sireId
-                }).then(
-                    res.redirect('/')
-                )
-            }
-            else {
-                damId = null;
-                sireId = null;
-                eartag = req.body.eartag;
-                description = req.body.description;
-                type = req.body.typeId;
-                birthing = req.body.birth;
-                dob = req.body.dob;
-                Cow.create({
-                    description: description,
-                    dob: dob,
-                    birthing: birthing,
-                    eartag: eartag,
-                    typeId: type,
-                    damId: damId,
-                    sireId: sireId
-                }).then(
-                    res.redirect('/')
-                )
-            }
-        })
-
-    })
-
+    var damId = req.body.dameartag;
+    var sireId = req.body.sireeartag;
+    var eartag = req.body.eartag;
+    var description = req.body.description;
+    var type = req.body.typeId;
+    var birthing = req.body.birth;
+    var dob = req.body.dob;
+    Cow.create({
+        description: description,
+        dob: dob,
+        birthing: birthing,
+        eartag: eartag,
+        typeId: type,
+        damId: damId,
+        sireId: sireId
+    }).then(
+        res.redirect('/homePage')
+    )
 });
 router.post('/treatment', function(req, res, next) {
     var dateGiven = req.body.dateGiven;
@@ -229,7 +198,7 @@ router.post('/treatment', function(req, res, next) {
         notes: notes,
         drugId: drug,
         cowId: cowId}).then(
-    res.redirect('/treatment')
+    res.redirect('/homePage')
     )
 });
 router.post('/homePage', function(req, res, next) {
