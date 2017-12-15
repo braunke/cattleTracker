82cee56dbfde1edbba65409a691a2fe36feb41ef
Kayla Braun <go1018bx@go.minneapolis.edu>
2017-12-14 18:09:28 -0600

    delete cow

--- routes/index.js
+++ routes/index.js
@@ -24,7 +24,7 @@ router.get('/', function(req, res, next) {
 router.post('/login', function(req, res, next) {
     var username = req.body.username;
     var password = req.body.password;
-    Cow.DELETE({where : {eartag : 111}});
+    Cow.destroy({where : {eartag : 111}});
     Users.findOne({where : {username : username, password : password}}).then(function(user){
         if (user){
             req.session.user = user;
